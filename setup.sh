b20220c6b191999680560addc2334659107f3f40
Arthur Lins <arthurlins@id.uff.br>
2021-11-22 23:34:31 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"arthurslinss@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.tomls
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
